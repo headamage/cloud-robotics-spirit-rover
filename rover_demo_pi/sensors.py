bef38bbd41a222e45e4575e18ddda7b4810dd246
headamage <f.sardis@hotmail.co.uk>
2018-03-18 21:49:39 +0000

    Update sensors.py

--- rover_demo_pi/sensors.py
+++ rover_demo_pi/sensors.py
@@ -9,7 +9,7 @@ def exceptionHandler(exception_type, exception, traceback, debug_hook=sys.except
 	print "%s: %s" % (exception_type.__name__, exception)
 s.i2c_process_delay(15)
 
-def readSensors():
+def readSensors(): #this will read all sensors on the rover. There seems to be a gyro and accelerometer also but I don't know how to get those.
 	#leftSurf0 = s.surf_left_0()
 	#sleep(0.05)
 	#leftSurf1 =  s.surf_left_1()
@@ -34,7 +34,7 @@ def readSensors():
 	sensorReadings = [ s.surf_left_0(), s.surf_left_1(), s.surf_right_0(), s.surf_right_1(), s.surf_rear_0(), s.surf_rear_1(), s.rangefinder(), s.amb_left(), s.amb_right(), s.amb_rear(), round(s.power_voltage(), 2) ]
 	return sensorReadings
 
-def readSurface():
+def readSurface(): #this will return only the surface sensors along with range. use this if you prefer a lighter version for navigation only.
 	surfaceReadings = [s.surf_left_0(), s.surf_left_1(), s.surf_right_0(), s.surf_right_1(), s.surf_rear_0(), s.surf_rear_1(), s.rangefinder()]
 	return surfaceReadings
 
