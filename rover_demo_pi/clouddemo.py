919a3ca81bd1cb4c88f0874f8e87833ed7fba85e
headamage <f.sardis@hotmail.co.uk>
2018-03-18 19:09:28 +0000

    Update clouddemo.py

--- rover_demo_pi/clouddemo.py
+++ rover_demo_pi/clouddemo.py
@@ -1,3 +1,6 @@
+#you can put this script in rc.local to automatically launch it when the pi boots but it is not recommended since it will impact battery life.
+#It may also interfere with anything else you are trying to do with the rover. Only run it when you want to experiment with cloud robotics.
+
 import socket, sys, time, spirit_core
 import sensors as sensors
 from time import sleep
@@ -9,16 +12,16 @@ def exceptionHandler(exception_type, exception, traceback, debug_hook=sys.except
 s.i2c_process_delay(15)
 
 # Network variables
-rover_addr = ("192.168.11.13", 10000)
-server_addr = ("192.168.11.21", 10000)
-graph_addr = ("192.168.11.21", 10001)
+rover_addr = ("192.168.11.13", 10000) #set this to match the IP of your rover
+server_addr = ("192.168.11.21", 10000) #set this to match the IP of the PC that will run your AI or manual control scripts
+graph_addr = ("192.168.11.21", 10001) #set this to match the IP of the PC that will run your sensor graphs. Typically same as the control PC.
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket
 sock.bind(rover_addr)
 
-def netSens():
-	try:
+def netSens(): #this will constantly read all sensors and send them over wifi to the PC that runs graphs and control.
+	try: #lets turn on some pixels to indicate our script is live. Some pixels will light blue on left and right wings.
 		pixels.hue_pixel(12,240,5)
 		sleep(0.1)
 		pixels.hue_pixel(24,240,5)
@@ -37,7 +40,7 @@ def netSens():
 	except KeyboardInterrupt:
 		print "exiting net sense"
 
-def netCtrl():
+def netCtrl(): #this will constantly listen on the pi address for motor and servo instructions.
 	try:
 		s.servo_speed(4000)
 		pong = 0
@@ -75,15 +78,15 @@ def netCtrl():
 
 
 if __name__ == '__main__':
-	try:
-		parent_conn, child_conn = Pipe()
+	try: #lets give a thread for each function so they run independently and improve the performance of the pi a bit.
+		#parent_conn, child_conn = Pipe()
 		p1 = Process(target=netSens)
 		p1.start()
 		p2 = Process(target=netCtrl)
 		p2.start()
 		p1.join()
 		p2.join()
-	except KeyboardInterrupt:
+	except KeyboardInterrupt: #let's turn off the blue pixels on the wing to indicate that we are not active anymore.
 		print "Terminating main thread"
 		s.motors(0,0)
 		sleep(0.1)
